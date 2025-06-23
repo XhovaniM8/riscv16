f4f87d62b4fe7f9a7fb99d6faf61e455defc7a42
Xhovani Mali <xhovani03@live.com>
2025-06-23 10:06:05 -0400

    Update instr_memory.sv

--- src/instr_memory.sv
+++ src/instr_memory.sv
@@ -1,3 +1,10 @@
+/***************************************************************************
+ *
+ * Module:  instr_memory.sv
+ *
+ * Description: This module implements an 16b instruction memory
+ *
+ ****************************************************************************/
 module instr_memory(
     input logic [15:0] pc,
     output logic [15:0] instr
