88f4b8c88ab4fa256d2c962538c2a3bd48329fed
Xhovani Mali <xhovani03@live.com>
2025-06-23 09:59:30 -0400

    Create instr_memory.sv

---
+++ src/instr_memory.sv
@@ -0,0 +1,9 @@
+module instr_memory(
+    input logic [15:0] pc,
+    output logic [15:0] instr
+);
+    reg [15:0] memory[0:255];
+    assign instr = memory[pc[7:0]];
+
+    initial $readmemb("program.mem", memory);
+endmodule
